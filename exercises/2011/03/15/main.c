f9831165739008422e077929affb056474d597fa
Guido De Rosa <job@guidoderosa.net>
2011-03-16 19:52:56 +0100

    cholesky: fix main.c style

--- exercises/2011/03/15/main.c
+++ exercises/2011/03/15/main.c
@@ -14,11 +14,16 @@
 
 int main() {
 	Matrix A, L;	
+	
 	tridiag_simmetric_positive_init(A);
+
+	cholesky_L(L, A);
+	
 	printf("A = \n");
 	matrix_print(A);
 
 	printf("\nL= \n");
-	cholesky_L(L, A);
 	matrix_print(L);
+
+	return 0;
 }
