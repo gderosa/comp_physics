d388d2c8deb30dea24ea70a972ee077f4bb81eeb
Guido De Rosa <job@guidoderosa.net>
2011-03-29 03:18:02 +0200

    Revert "integral/Binary (cached values): bad performance :-("
    This reverts commit a4e66258b038c00ca6ddb0c40b26696fca301598.

--- exercises/2011/03/25/pendulum/pendulum_multi.cpp
+++ exercises/2011/03/25/pendulum/pendulum_multi.cpp
@@ -6,7 +6,7 @@
 
 #include "function/Base.h"
 //#include "integral/Trapezoidal.h"
-#include "integral/BinarySimpson.h"
+#include "integral/Simpson.h"
 #include "interpolator/Polynomial.h"
 
 using namespace std;
@@ -20,23 +20,27 @@ struct PendulumIntegrand : public function::Base
 
   double operator()(const double theta) 
   {
-    return 1 / sqrt( cos(theta) - cos(theta_0) + 1e-10);
-    //return 1.0;
+    return 1 / sqrt( cos(theta) - cos(theta_0) );
   }
 };
 
 double integrate(const double theta_0) 
 {
   PendulumIntegrand f(theta_0);
-  integral::BinarySimpson binaryIntegral(&f, 0.0, theta_0, 8);
+  integral::Simpson integral(&f);
+  unsigned int n; 
   vector< pair<double, double> > extrapolation_points;
 
-  while (binaryIntegral.order < 22) { 
+  integral.lowerEnd = 0.0;
+
+  for (n = (1 << 8); n < (1 << 22); n *= 2) { 
+    integral.nIntervals = n - 1;
+    // exclude the last subinterval, where the f diverges
+    integral.upperEnd = theta_0 * ((double)(n - 1)/(double)n); 
     pair<double, double> point(
-        binaryIntegral.deltaX(), binaryIntegral.compute() 
+        integral.deltaX(), integral.compute() 
     );
     extrapolation_points.push_back(point);
-    binaryIntegral.refineDiscretization();
   }
 
   interpolator::Polynomial intpl(extrapolation_points);
